816513509fceb07592eb8c579a8804b1d01bf596
yasminawed <117314922+yasminawed@users.noreply.github.com>
2025-03-07 15:07:19 +0100

    Create tenant_main.dart

---
+++ flutter_project/lib/tenant_main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'pages_tenant/home_page.dart';
+import 'pages_tenant/feedback_page.dart';
+import 'pages_tenant/suggestions_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IEQ Tenant Interface',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    SuggestionsPage(),
+    FeedbackPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.lightbulb), label: "Suggestions"),
+          BottomNavigationBarItem(icon: Icon(Icons.feedback), label: "Feedback"),
+        ],
+      ),
+    );
+  }
+}
