3e316ff66b6d3b9541c63ec26d8702ea6ab0f7cb
yasminawed <117314922+yasminawed@users.noreply.github.com>
2025-04-01 12:51:00 +0200

    Update main.dart

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart'; // 👈 Importa la nuova pagina di login
+import 'start_screen.dart';
 
 void main() {
-  runApp(MainSelectorApp());
+  runApp(const MainSelectorApp());
 }
 
 class MainSelectorApp extends StatelessWidget {
+  const MainSelectorApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'IEQ App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(), // 👈 Mostra la login all'avvio
+      home: const StartScreen(),
     );
   }
 }
