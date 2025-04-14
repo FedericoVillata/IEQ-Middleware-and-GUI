083da1b97d71bd2b2f7171a639edab4d75afcd2a
Pulin98 <federicovillata1998@gmail.com>
2025-04-14 19:00:58 +0200

    correct web release

--- web-release/web/flutter_service_worker.js
+++ web-release/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "310b5bab6ff7081551312409f02500bf
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f9235162af111e43223665ce283efec3",
+"flutter_bootstrap.js": "64af2c3cc3f1ccb542d533217e26a6f0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
