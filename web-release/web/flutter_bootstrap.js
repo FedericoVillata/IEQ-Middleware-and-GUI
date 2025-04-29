ba0dd1e265a6970354c57990dda0306e2b469b42
Pulin98 <federicovillata1998@gmail.com>
2025-04-29 10:27:07 +0200

    web

--- web-release/web/flutter_bootstrap.js
+++ web-release/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2986947987"
+    serviceWorkerVersion: "2949541081"
   }
 });
